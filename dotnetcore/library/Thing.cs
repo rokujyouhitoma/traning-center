acc482042fae86738869d16c123a31c6613dc2e6
rokujyouhitoma <rokujyouhitomajp@gmail.com>
2018-07-08 22:51:49 +0900

    Implements of library/Thing.cs

---
+++ dotnetcore/library/Thing.cs
@@ -0,0 +1,11 @@
+﻿using static Newtonsoft.Json.JsonConvert;
+using System;
+
+namespace Library
+{
+    public class Thing
+    {
+        public int Get(int left, int right) =>
+            DeserializeObject<int>($"{left + right}");
+    }
+}
